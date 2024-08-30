d7e4c5b40a8310f0a5029f642e14d33cda34c9ba
nielsvanderknaap <niels.vander.knaap@capgemini.com>
2024-08-30 15:23:52 +0200

    [TASK] Adds delete functionality

--- MSNightMauiDemo/ViewModels/ShoppingListViewModel.cs
+++ MSNightMauiDemo/ViewModels/ShoppingListViewModel.cs
@@ -1,11 +1,13 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MSNightMauiDemo.Observables;
 
 namespace MSNightMauiDemo.ViewModels;
 
 public partial class ShoppingListViewModel : ObservableObject
 {
+    private IRelayCommand<Item> _deleteItemCommand;
     [ObservableProperty]
     private ObservableCollection<Item> _shoppingList = new ObservableCollection<Item>();
 
@@ -16,5 +18,15 @@ public partial class ShoppingListViewModel : ObservableObject
             Name = "Melk",
             Count = 2
         });
+        ShoppingList.Add(new Item
+        {
+            Name = "Eieren",
+            Count = 2
+        });
     }
+
+    public IRelayCommand<Item> DeleteItemCommand => _deleteItemCommand ?? new RelayCommand<Item>((item) =>
+    {
+        ShoppingList.Remove(item);
+    });
 }
