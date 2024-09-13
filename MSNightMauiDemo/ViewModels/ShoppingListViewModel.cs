9d213ce01cd476e4925ebe064a98b6e80e859339
nielsvanderknaap <niels.vander.knaap@capgemini.com>
2024-09-13 15:53:50 +0200

    [TASK] Increment and subtract shopping items

--- MSNightMauiDemo/ViewModels/ShoppingListViewModel.cs
+++ MSNightMauiDemo/ViewModels/ShoppingListViewModel.cs
@@ -8,6 +8,8 @@ namespace MSNightMauiDemo.ViewModels;
 public partial class ShoppingListViewModel : ObservableObject
 {
     private IRelayCommand<Item> _deleteItemCommand;
+    private IRelayCommand<Item>? _incrementItemCommand;
+    private IRelayCommand<Item>? _subtractItemCommand;
     [ObservableProperty]
     private ObservableCollection<Item> _shoppingList = new ObservableCollection<Item>();
 
@@ -34,5 +36,22 @@ public partial class ShoppingListViewModel : ObservableObject
     public IRelayCommand<Item> DeleteItemCommand => _deleteItemCommand ?? new RelayCommand<Item>((item) =>
     {
         ShoppingList.Remove(item);
+    });
+    
+    public IRelayCommand<Item> IncrementItemCommand => _incrementItemCommand ?? new RelayCommand<Item>((item) =>
+    {
+        item.Count++;
+    });
+    
+    public IRelayCommand<Item> SubtractItemCommand => _subtractItemCommand ?? new RelayCommand<Item>((item) =>
+    {
+        if (item.Count == 1)
+        {
+            DeleteItemCommand.Execute(item);
+        }
+        else
+        {
+            item.Count--;
+        }
     });
 }
